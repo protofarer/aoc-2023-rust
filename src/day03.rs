89bd0795a0d074c77c90cf84b9026ca4008def92
Kenny Baron <kennybaron@fastmail.com>
2023-12-05 15:01:48 -0500

    fin 3-2

--- src/day03.rs
+++ src/day03.rs
@@ -30,9 +30,6 @@ impl Group {
     fn len(&self) -> usize {
         self.coords.len()
     }
-    fn print(&self) {
-        println!("{}", self.digits);
-    }
 }
 
 // SOLN: 522726
@@ -160,11 +157,76 @@ fn first(input: &mut dyn BufRead) -> String {
     sum.to_string()
 }
 
-// SOLN:
+// SOLN: 81721933
+// 3607 low
 fn second(input: &mut dyn BufRead) -> String {
-    for line in get_string_from_input(input).lines() {}
+    // for each '*' with exactly 2 adjacent digit groups, multiply the digit group values with each other
+    // sum the mults
 
-    "".to_string()
+    let mut digit_groups: Vec<Group> = vec![];
+    let mut star_coords: Vec<Coord> = vec![];
+
+    let mut row = 0;
+    for line in get_string_from_input(input).lines() {
+        let mut group = Group::new();
+        for (col, c) in line.char_indices() {
+            if c.is_numeric() {
+                group.add_number(c, row, col);
+            } else {
+                if group.len() > 0 {
+                    digit_groups.push(group.clone());
+                    group = Group::new();
+                }
+                if c == '*' {
+                    star_coords.push(Coord {
+                        row: row as i32,
+                        col: col as i32,
+                    });
+                }
+            }
+        }
+        if group.len() > 0 {
+            digit_groups.push(group.clone());
+        }
+        row += 1;
+    }
+
+    let mut sum = 0;
+    'stars: for star in star_coords {
+        let (star_row, star_col) = (star.row, star.col);
+
+        let mut count = 0;
+        let mut digits = vec![];
+
+        for d_row in -1..=1 {
+            for d_col in -1..=1 {
+                if d_row == 0 && d_col == 0 {
+                    continue;
+                }
+
+                // as soon as a digit from a part number group is found adjacent, add to digits and remove from digit_groups so it won't be iterated over again (counted twice)
+                if let Some(idx) = digit_groups.iter().position(|group| {
+                    group
+                        .coords
+                        .iter()
+                        .any(|coord| coord.row == star_row + d_row && coord.col == star_col + d_col)
+                }) {
+                    let group = digit_groups.remove(idx);
+                    digits.push(group.digits.to_owned());
+                    count += 1;
+                }
+
+                if count > 2 {
+                    continue 'stars;
+                }
+            }
+        }
+        if count == 2 {
+            sum += digits[0].parse::<u32>().unwrap() * digits[1].parse::<u32>().unwrap();
+        }
+    }
+
+    sum.to_string()
 }
 
 pub const SOLVERS: &[Solver] = &[first, second];
