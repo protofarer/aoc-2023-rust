d1a12ff476d2688a9d2ef0b4490f6e8b56425ecb
Kenny Baron <kennybaron@fastmail.com>
2023-12-14 15:15:44 -0500

    fin 6-2

--- src/day06.rs
+++ src/day06.rs
@@ -46,11 +46,52 @@ fn first(input: &mut dyn BufRead) -> String {
     result.to_string()
 }
 
-// SOLN:
+// SOLN: 26187338
+// 42686982 high (confused distance_record with duration)
 fn second(input: &mut dyn BufRead) -> String {
-    for line in get_string_from_input(input).lines() {}
+    const SPEED_INCREMENT: u64 = 1; // 1 millimeter per millisecond
 
-    "".to_string()
+    let lines_string = get_string_from_input(input);
+    let mut lines = lines_string.lines();
+    let first_line = lines.next().unwrap();
+
+    let race_duration = first_line
+        .split_once(':')
+        .unwrap_or(("", ""))
+        .1
+        .trim()
+        .replace(" ", "")
+        .parse::<u64>()
+        .unwrap();
+
+    println!("time: {}", race_duration);
+
+    let second_line = lines.next().unwrap();
+
+    let distance_record = second_line
+        .split_once(':')
+        .unwrap_or(("", ""))
+        .1
+        .trim()
+        .replace(" ", "")
+        .parse::<u64>()
+        .unwrap();
+
+    println!("distance: {}", distance_record);
+
+    let mut ways = 0;
+    for t_held in 0..race_duration {
+        let t_move = race_duration - t_held;
+        let speed = t_held * SPEED_INCREMENT;
+        let distance = speed * t_move;
+        if distance > distance_record {
+            ways += 1;
+        }
+    }
+    // let result: u32 = ways_per_race.iter().product();
+
+    // result.to_string()
+    ways.to_string()
 }
 
 pub const SOLVERS: &[Solver] = &[first, second];
